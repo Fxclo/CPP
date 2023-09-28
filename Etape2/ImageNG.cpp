c690dafd75ee191c5e27f1d3c6a72c2994c5ece5
Fxclo <pompeodattoli4570@gmail.com>
2023-09-28 16:16:03 +0200

    :memo: Creation readme.md

---
+++ Etape2/ImageNG.cpp
@@ -0,0 +1,85 @@
+#include "ImageNG.h"
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+ImageNG::ImageNG() // constructeur par défaut
+{
+  nom = NULL;
+  setNom("default");
+  setId(1);
+}
+
+ImageNG::~ImageNG() // destructeur
+{
+  if(nom) delete nom;
+}
+
+ImageNG::ImageNG(int id, const char* N) // constructeur d'initialisation
+{
+  nom = NULL;
+  setNom(N);
+  setId(id);
+}
+
+ImageNG::ImageNG(const ImageNG& image1) // constructeur de copie
+{
+  nom = NULL;
+  setNom(image1.getNom());
+  setId(image1.getId());
+}
+
+ImageNG::ImageNG(int id, const char* N, Dimension dim) 
+{
+  nom = NULL;
+  setNom(N);
+  setId(id);
+
+  setDimension(dim);
+}
+
+void ImageNG::setDimension(Dimension dim)
+{
+  dimension = dim;
+}
+
+
+void ImageNG::setId(int val)
+{
+  if (val >= 0)
+    id = val;
+}
+
+void ImageNG::setNom(const char* N)
+{
+  if (strlen(N)== 0) // si la chaine est vide
+    return;
+
+  if (nom) delete nom;
+
+  nom = new char[strlen(N)+1]; // on alloue dynamiquement
+  strcpy(nom, N);
+}
+
+Dimension ImageNG::getDimension() const
+{
+  return dimension;
+}
+
+int ImageNG::getId() const
+{
+  return id; 
+}
+
+const char* ImageNG::getNom() const
+{
+  return nom;
+}
+
+void ImageNG::Affiche() const
+{
+  cout << "Nom : " << getNom() << endl;
+  cout << "ID : " << getId() << endl;
+  dimension.Affiche();
+}
---
+++ Etape2/ImageNG.h
@@ -0,0 +1,34 @@
+#ifndef IMAGE_H
+#define IMAGE_H
+#include "Dimension.h"
+
+class ImageNG
+{
+
+private:
+
+  int id;
+  char* nom;
+  Dimension dimension;
+
+public:
+
+  ImageNG(); // constructeur par défaut
+  ~ImageNG(); // destructeur
+  ImageNG(int id, const char* N); // constructeur d'initialisation
+  ImageNG(const ImageNG& image1); // constructeur de copie
+  ImageNG(int id, const char* N, Dimension dim); // constructeur d'initialisation complet
+
+  void setDimension(Dimension dim);
+  void setId(int val);
+  void setNom(const char* N);
+
+  Dimension getDimension()const;
+  int getId()const;
+  const char* getNom()const;
+
+  void Affiche()const;
+
+};
+
+#endif
